9876dc1c07e98a685c0f69c5f70bdc62ad72f086
linhu <1205668006@qq.com>
2020-03-17 13:49:58 +0800

    单例模式

---
+++ javamodel/src/main/java/com/study/javamodel/JavamodelApplication.java
@@ -0,0 +1,13 @@
+package com.study.javamodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamodelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavamodelApplication.class, args);
+    }
+
+}
