195da331824346dd356a4a74e8a2bd347c6b5e28
linhu <1205668006@qq.com>
2020-04-23 18:25:02 +0800

    单例模式

---
+++ myjson-spring-boot-starter/src/main/java/com/mystarter/myjsonspringbootstarter/MyjsonSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.mystarter.myjsonspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjsonSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyjsonSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ myspringall/src/test/java/com/my/web/tiger/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.my.web.tiger;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
