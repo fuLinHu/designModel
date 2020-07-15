a9c5b29c914001cb05f0fcaeea4470c0bdca5bed
linhu <1205668006@qq.com>
2020-07-15 19:33:54 +0800

    autho2完结

---
+++ parent/vueserver/sso-portal-password-vue/src/main/java/com/tuling/SsoPortalPasswordVueApplication.java
@@ -0,0 +1,13 @@
+package com.tuling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoPortalPasswordVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoPortalPasswordVueApplication.class, args);
+	}
+
+}
