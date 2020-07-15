b5b171832c4dc0f3fd88570881687545ad52f0e5
linhu <1205668006@qq.com>
2020-07-15 19:32:28 +0800

    autho2完结

---
+++ parent/sentinel/tulingvip05-ms-alibaba-ribbonwithsentinel-order/src/main/java/com/tuling/v1/controller/OrderInfoController.java
@@ -0,0 +1,59 @@
+package com.tuling.v1.controller;
+
+import com.tuling.entity.OrderInfo;
+import com.tuling.entity.ProductInfo;
+import com.tuling.mapper.OrderInfoMapper;
+import com.tuling.vo.OrderVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+
+/**
+ * Created by smlz on 2019/11/17.
+ */
+@RestController
+public class OrderInfoController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private OrderInfoMapper orderInfoMapper;
+
+    @RequestMapping("/selectOrderInfoById/{orderNo}")
+    public Object selectOrderInfoById(@PathVariable("orderNo") String orderNo) {
+        OrderInfo orderInfo = new OrderInfo();
+        orderInfo.setProductCount(10);
+        orderInfo.setUserName("flh");
+        orderInfo.setCreateDt(new Date());
+        orderInfo.setProductNo("1");
+        orderInfo.setOrderNo("2");
+        //OrderInfo orderInfo = orderInfoMapper.selectOrderInfoById(orderNo);
+        if(null == orderInfo) {
+            return "根据orderNo:"+orderNo+"查询没有该订单";
+        }
+
+        ResponseEntity<ProductInfo> responseEntity= restTemplate.getForEntity("http://product-center/selectProductInfoById/"+orderInfo.getProductNo(), ProductInfo.class);
+
+        ProductInfo productInfo = responseEntity.getBody();
+
+        if(productInfo == null) {
+            return "没有对应的商品";
+        }
+
+        OrderVo orderVo = new OrderVo();
+        orderVo.setOrderNo(orderInfo.getOrderNo());
+        orderVo.setUserName(orderInfo.getUserName());
+        orderVo.setProductName(productInfo.getProductName());
+        orderVo.setProductNum(orderInfo.getProductCount());
+
+        return orderVo;
+    }
+
+
+}
--- parent/sso/tulingvip-ms-alibaba-common/src/main/java/com/tuling/entity/ProductInfo.java
+++ parent/sso/tulingvip-ms-alibaba-common/src/main/java/com/tuling/entity/ProductInfo.java
@@ -2,17 +2,19 @@ package com.tuling.entity;
 
 import lombok.Data;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Created by smlz on 2019/11/17.
  */
 @Data
 public class ProductInfo {
 
-    private String productNo;
+    private String productNo= "001";
 
-    private String productName;
+    private String productName= "手表";
 
-    private String productStore;
+    private String productStore = "北京仓库";
 
-    private double productPrice;
+    private double productPrice=100;
 }
