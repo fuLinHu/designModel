8f0628a895dd745dc9df14809668fea7bf92b24a
linhu <1205668006@qq.com>
2020-08-19 19:54:40 +0800

    DateFormat  线程不安全

---
+++ javamodel/src/main/java/com/study/javamodel/javaapi/SimpleDateformat/DateFormatUtil.java
@@ -0,0 +1,45 @@
+package com.study.javamodel.javaapi.SimpleDateformat;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * @className
+ * @Description TODO
+ * @Author 付林虎
+ * @Date 2020/8/19 17:57
+ * @Version V1.0
+ */
+public class DateFormatUtil {
+    private static ThreadLocal<DateFormat> threadLocal = new ThreadLocal();
+    public static final DateFormat getDateFormat(String patter){
+        DateFormat dateFormat = threadLocal.get();
+        if(dateFormat==null){
+            threadLocal.set(new SimpleDateFormat(patter));
+        }
+        return threadLocal.get();
+    }
+    public static void remove(){
+        threadLocal.remove();
+    }
+
+
+    public static Date convertStr2Date(String strDate, String dateFormatter, ZoneIdEnum timezone) {
+        DateTimeFormatter ofPattern = DateTimeFormatter.ofPattern(dateFormatter);
+        LocalDate parseDate = LocalDate.parse(strDate,ofPattern);
+        ZoneId zone = ZoneId.of(timezone.getZoneId());
+        Instant instant = parseDate.atStartOfDay().atZone(zone).toInstant();
+        return Date.from(instant);
+    }
+
+    public static String convertDate2Str(String dateFormatter,long date) {
+        LocalDateTime dateTime = LocalDateTime.ofEpochSecond(date / 1000L, 0, ZoneOffset.ofHours(8));
+        String time  = dateTime.format(DateTimeFormatter.ofPattern(dateFormatter));
+        return time;
+    }
+
+
+}
---
+++ javamodel/src/main/java/com/study/javamodel/javaapi/SimpleDateformat/TestSimpleDateformat.java
@@ -0,0 +1,59 @@
+package com.study.javamodel.javaapi.SimpleDateformat;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * @className
+ * @Description TODO
+ * @Author 付林虎
+ * @Date 2020/8/19 16:52
+ * @Version V1.0
+ */
+public class TestSimpleDateformat {
+
+
+
+    public static void main(String[] args) {
+        //test2();
+        System.out.println(DateFormatUtil.convertDate2Str("yyyy-MM-dd HH:mm:ss", new Date().getTime()));
+    }
+
+    public static void test1(){
+        for (int i = 0; i <1000 ; i++) {
+            new Thread(()->{
+                DateFormat dateFormat = DateFormatUtil.getDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date parse = null;
+                try {
+                    parse = dateFormat.parse("2020-07-08 00:00:00");
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }finally {
+                    DateFormatUtil.remove();
+                }
+                System.out.println(parse.toString());
+            }).start();
+        }
+    }
+
+
+
+
+    public static void test2(){
+        for (int i = 0; i <1000 ; i++) {
+            new Thread(()->{
+                Date date = DateFormatUtil.convertStr2Date("2020-07-08 00:00:00", "yyyy-MM-dd hh:mm:ss", ZoneIdEnum.ASIA_SHANGHAI);
+                System.out.println(date.toString());
+            }).start();
+        }
+    }
+
+
+
+}
