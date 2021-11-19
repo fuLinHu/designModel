9358fd157a6373038a924a8501916cbf380dc986
linhu <1205668006@qq.com>
2021-11-19 14:48:25 +0800

    微服务

---
+++ tiger/tiger/src/main/java/com/forest/tiger/rtemplateroot/TigerRootApplication.java
@@ -0,0 +1,13 @@
+package com.forest.tiger.rtemplateroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TigerRootApplication.class, args);
+    }
+
+}
---
+++ tiger/tiger/src/test/java/com/forest/tiger/rtemplateroot/RtemplaterootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forest.tiger.rtemplateroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtemplaterootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
